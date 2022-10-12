8534fba0a036721f002e28cf8d17835e89b1acf1
Tina <menetina7@gmail.com>
2022-10-12 07:46:03 -0700

    0-print_name.c

--- 0-print_name.c/0-print_name.c
+++ 0-print_name.c/0-print_name.c
@@ -9,14 +9,15 @@
  *
  *    * @f: function
  *
- *  Return: void.
  *     */
 
 void print_name(char *name, void (*f)(char *))
 
 {
-	if (name == NULL || f == NULL)
 
-	f(name);
+		if (name == NULL || f == NULL)
 
+			return;
+
+			f(name);
 }
