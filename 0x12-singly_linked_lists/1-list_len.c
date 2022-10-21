9b7a6f6de8a23de81328056ff4ce5a39f8f477ad
Distinct20 <98983531+Distinct20@users.noreply.github.com>
2022-10-21 22:31:35 +0100

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,21 @@
 #include "lists.h"
-
 /**
- * list_len - Returns the number of elements in a list.
- * @h: singly linked list.
- * Return: The number of elements in the list.
- */
+* list_len - a function that returns the number
+*  of elements in a linked list_t list
+* @h: input
+* Return: 0
+*/
 size_t list_len(const list_t *h)
 {
-	size_t number of nodes;
-        number_of_nodes= 0;
-	while (h !=NULL)
-	{
-		h = h->next;
-		number_of_nodes++;
-	}
+	int len = 0;
 
-	return (nodes);
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
 }
